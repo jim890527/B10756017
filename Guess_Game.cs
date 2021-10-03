1067c122d310a37fb0abe1a127d577b43489ae64
Jun Yi <67197892+jim890527@users.noreply.github.com>
2021-10-04 01:19:45 +0800

    Add files via upload

---
+++ Guess_Game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Guess_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ans;
+            do
+            {
+                Console.WriteLine("Guess Game:\r\nQuestion:NBA2019-2020賽季中因何事延後賽程？\r\n1.SARS 2.H1N1 3.HIV 4.AIDS 5.天花 6.霍亂 7.鼠疫 8.COVID-19 9.瘧疾 10.結核病");
+                ans = Console.ReadLine();
+                int a = int.Parse(ans);
+                if (a >= 1 && a <= 10)
+                {
+                    if (ans != "8")
+                        Console.WriteLine("wrong answer\r\n");
+                    else
+                        Console.WriteLine("right answer");
+                }
+                else
+                    Console.WriteLine("輸入錯誤請重新輸入\r\n");
+            }while(ans != "8");
+            Console.ReadLine();
+        }
+    }
+}
